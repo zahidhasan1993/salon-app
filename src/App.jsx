7645dc7dee1ccfd351c18f76038c3cf694eb51b3
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-18 14:54:46 +0600

    adding category section for the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
 import Banner from "./components/Banner";
+import Category from "./components/Category";
 import Navbar from "./components/Navbar";
+import img1 from "./assets/category-images/1.jpg"
+import img2 from "./assets/category-images/2.jpg"
+import img3 from "./assets/category-images/3.jpg"
+import img4 from "./assets/category-images/4.jpg"
+import img5 from "./assets/category-images/5.jpg"
+
 
 const App = () => {
+  const categoryItems = [
+    {
+      name: "SKIN",
+      img : img1
+    },
+    {
+      name: "HAIR",
+      img : img2
+    },
+    {
+      name: "MAKEUP",
+      img : img3
+    },
+    {
+      name: "TIPS",
+      img : img4
+    },
+    {
+      name: "SPA",
+      img : img5
+    },
+  ]
   return (
     <div className="">
       <Navbar></Navbar>
       <Banner></Banner>
+      <Category></Category>
       
     </div>
   );
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,13 @@
-
 const Category = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="relative">
+      <img src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80" alt="Image" className="w-full h-auto" />
+
+      <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white bg-black bg-opacity-50 p-4 rounded">
+        Your Text
+      </p>
+    </div>
+  );
 };
 
 export default Category;
