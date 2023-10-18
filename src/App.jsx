ad85357b192c7c8eb26fe2a5554366ae1fb37f9f
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-18 23:55:37 +0600

    adding a section title to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,55 @@
 import Banner from "./components/Banner";
 import Category from "./components/Category";
 import Navbar from "./components/Navbar";
-import img1 from "./assets/category-images/1.jpg"
-import img2 from "./assets/category-images/2.jpg"
-import img3 from "./assets/category-images/3.jpg"
-import img4 from "./assets/category-images/4.jpg"
-import img5 from "./assets/category-images/5.jpg"
-
+import img1 from "./assets/category-images/1.jpg";
+import img2 from "./assets/category-images/2.jpg";
+import img3 from "./assets/category-images/3.jpg";
+import img4 from "./assets/category-images/4.jpg";
+import img5 from "./assets/category-images/5.jpg";
 
 const App = () => {
   const categoryItems = [
     {
       id: 1,
       name: "SKIN",
-      img : img1
+      img: img1,
     },
     {
       id: 2,
       name: "HAIR",
-      img : img2
+      img: img2,
     },
     {
       id: 3,
       name: "MAKEUP",
-      img : img3
+      img: img3,
     },
     {
       id: 4,
       name: "TIPS",
-      img : img4
+      img: img4,
     },
     {
       id: 5,
       name: "SPA",
-      img : img5
+      img: img5,
     },
-  ]
+  ];
   return (
     <div className="">
       <Navbar></Navbar>
       <Banner></Banner>
       <div className="container mx-auto grid grid-cols-1 gap-5 md:grid md:grid-cols-5 md:gap-8 mt-12 w-full">
-        {
-          categoryItems.map(item => <Category key={item.id} item={item}></Category>)
-        }
+        {categoryItems.map((item) => (
+          <Category key={item.id} item={item}></Category>
+        ))}
       </div>
-      
+      {/* divider to latest post */}
+      <div className="my-20 container mx-auto flex justify-center items-center gap-0">
+        <div className="h-1 bg-black w-[40rem] mx-auto my-4"></div>
+        <p className="text-3xl ">Latest Post</p>
+        <div className="h-1 bg-black w-[40rem] mx-auto my-4"></div>
+      </div>
     </div>
   );
 };
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,9 +3,9 @@ const Category = ({ item }) => {
     <div className="relative">
       <img src={item.img} alt="Image" className="w-full h-[200px] opacity-70" />
       {/* adding black gradient on top of image*/}
-      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+      <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-60"></div>
       {/* adding text on top of image */}
-      <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 text-white py-3 px-5 rounded-3xl">
+      <p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 border-2 text-white py-2 px-6 rounded-3xl">
         {item.name}
       </p>
     </div>
