8e25c37115c7dbdba1951ec39c232e51bf2357a9
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-19 03:00:08 +0600

    Blog post component added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -10,50 +10,60 @@ import { FaArrowRight } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <Swiper navigation={true} modules={[Navigation]} className="mySwiper h-[50rem]">
+    <Swiper
+      navigation={true}
+      modules={[Navigation]}
+      className="mySwiper h-[50rem]"
+    >
       <SwiperSlide>
         <LazyLoadImage src={img2} className="w-full relative" />
-        <div className="h-[22rem] border-2 border-white absolute text-white top-[7rem] right-[10rem] p-8">
-          <p className="text-sm underline mb-5">SKIN</p>
-          <p className="font-new text-5xl mb-5">
-            Six Hyaluronic Serums <br /> For Glowing Summer Skin <br /> Vivamus
-            Placerat
-          </p>
-          <p className="mb-5">By Sarfraz Jasim - 29 July, 2023</p>
-          <a href="" className="flex items-center gap-2 ">
-            Continue Reading <FaArrowRight className="text-sm"></FaArrowRight>
-          </a>
-          <hr className="w-1/2 " />
+        <div className="absolute text-white top-[7rem] right-[10rem]  border-2 border-white px-20 py-14">
+          <div className="h-[22rem]">
+            <p className="text-sm underline mb-5">SKIN</p>
+            <p className="font-new text-5xl mb-5">
+              Six Hyaluronic Serums <br /> For Glowing Summer Skin <br />{" "}
+              Vivamus Placerat
+            </p>
+            <p className="mb-5">By Sarfraz Jasim - 29 July, 2023</p>
+            <a href="" className="flex items-center gap-2 ">
+              Continue Reading <FaArrowRight className="text-sm"></FaArrowRight>
+            </a>
+            <hr className="w-1/2" />
+          </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <LazyLoadImage src={img3} className="w-full relative" />
-        <div className="h-[22rem] border-2 border-white absolute text-white top-[7rem] right-[10rem] p-8">
-          <p className="text-sm underline mb-5">SKIN</p>
-          <p className="font-new text-5xl mb-5">
-            Six Hyaluronic Serums <br /> For Glowing Summer Skin <br /> Vivamus
-            Placerat
-          </p>
-          <p className="mb-5">By Sarfraz Jasim - 29 July, 2023</p>
-          <a href="" className="flex items-center gap-2 ">
-            Continue Reading <FaArrowRight className="text-sm"></FaArrowRight>
-          </a>
-          <hr className="w-1/2 " />
+        <div className="absolute text-white top-[7rem] right-[10rem]  border-2 border-white px-20 py-14">
+          <div className="h-[22rem]">
+            <p className="text-sm underline mb-5">SKIN</p>
+            <p className="font-new text-5xl mb-5">
+              Six Hyaluronic Serums <br /> For Glowing Summer Skin <br />{" "}
+              Vivamus Placerat
+            </p>
+            <p className="mb-5">By Sarfraz Jasim - 29 July, 2023</p>
+            <a href="" className="flex items-center gap-2 ">
+              Continue Reading <FaArrowRight className="text-sm"></FaArrowRight>
+            </a>
+            <hr className="w-1/2" />
+          </div>
         </div>
       </SwiperSlide>
       <SwiperSlide>
         <LazyLoadImage src={img1} className="w-full relative " />
-        <div className="h-[22rem] border-2 border-white absolute text-white top-[7rem] right-[10rem] p-8">
-          <p className="text-sm underline mb-5">SKIN</p>
-          <p className="font-new text-5xl mb-5">
-            Six Hyaluronic Serums <br /> For Glowing Summer Skin <br /> Vivamus
-            Placerat
-          </p>
-          <p className="mb-5">By Sarfraz Jasim - 29 July, 2023</p>
-          <a href="" className="flex items-center gap-2 ">
-            Continue Reading <FaArrowRight className="text-sm"></FaArrowRight>
-          </a>
-          <hr className="w-1/2 " />
+        <div className="absolute text-white top-[7rem] right-[10rem]  border-2 border-white px-20 py-14">
+          <div className="h-[22rem]">
+            <p className="text-sm underline mb-5">SKIN</p>
+            <p className="font-new text-5xl mb-5">
+              Six Hyaluronic Serums <br /> For Glowing Summer Skin <br />{" "}
+              Vivamus Placerat
+            </p>
+            <p className="mb-5">By Sarfraz Jasim - 29 July, 2023</p>
+            <a href="" className="flex items-center gap-2 ">
+              Continue Reading <FaArrowRight className="text-sm"></FaArrowRight>
+            </a>
+            <hr className="w-1/2" />
+          </div>
         </div>
       </SwiperSlide>
     </Swiper>
