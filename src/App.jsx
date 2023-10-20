8a3d3653b396aaebcc2c9dad33579e811f702190
devZahid <officialzahidhasan19932021@gmail.com>
2023-10-20 15:18:37 +0600

    some size fixing

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,10 @@ import img4 from "./assets/category-images/4.jpg";
 import img5 from "./assets/category-images/5.jpg";
 import popularImg1 from "./assets/blog-image/2.jpg";
 import popularImg2 from "./assets/blog-image/3.jpg";
-import instaImg1 from "./assets/blog-image/instagram-feed/ethan-haddox-NTjSR3zYpsY-unsplash.jpg"
-import instaImg2 from "./assets/blog-image/instagram-feed/pascal-brauer-kwiw8pjg9jw-unsplash.jpg"
-import instaImg3 from "./assets/blog-image/instagram-feed/wesley-tingey-LpZvsGynEho-unsplash.jpg"
-import instaImg4 from "./assets/blog-image/instagram-feed/zulmaury-saavedra-vh_pAs2FH_8-unsplash.jpg"
+import instaImg1 from "./assets/blog-image/instagram-feed/ethan-haddox-NTjSR3zYpsY-unsplash.jpg";
+import instaImg2 from "./assets/blog-image/instagram-feed/pascal-brauer-kwiw8pjg9jw-unsplash.jpg";
+import instaImg3 from "./assets/blog-image/instagram-feed/wesley-tingey-LpZvsGynEho-unsplash.jpg";
+import instaImg4 from "./assets/blog-image/instagram-feed/zulmaury-saavedra-vh_pAs2FH_8-unsplash.jpg";
 import randomImg1 from "./assets/blog-image/random-post/1.jpg";
 import randomImg2 from "./assets/blog-image/random-post/2.jpg";
 import randomImg3 from "./assets/blog-image/random-post/3.jpg";
@@ -94,14 +94,14 @@ const App = () => {
           {latest.map((item) => (
             <div key={item.id}>
               <div>
-                <div className="relative w-full">
+                <div className="md:relative w-full">
                   <LazyLoadImage
                     src={item.img}
                     alt="Image"
                     className="w-full h-72 rounded"
                   />
                   {/* adding black gradient on top of image*/}
-                  <div className="absolute top-48 left-12 w-4/5 bg-white p-8 text-center border border-black">
+                  <div className="md:absolute md:top-48 md:left-12 w-4/5 bg-white p-8 text-center border border-black">
                     <p className="text-gray-700 my-3 text-center underline">
                       {item.category}
                     </p>
@@ -130,10 +130,10 @@ const App = () => {
         <div className="absolute top-0 left-0 w-full h-full text-white px-8 py-5 flex justify-between items-center">
           <div className="">
             <div className="flex items-center gap-2 pb-2">
-              <FaConfluence className="font-bold text-xl"></FaConfluence>
-              <p className="font-bold text-2xl">PostX</p>
+              <FaConfluence className="font-bold md:text-xl"></FaConfluence>
+              <p className="font-bold md:text-2xl">PostX</p>
             </div>
-            <p className="font-bold text-5xl font-new ">
+            <p className="font-bold md:text-5xl font-new ">
               #1 Gutenberg Post Blocks
             </p>
           </div>
@@ -149,14 +149,14 @@ const App = () => {
           {popular.map((item) => (
             <div key={item.id}>
               <div>
-                <div className="relative w-full">
+                <div className="md:relative w-full">
                   <LazyLoadImage
                     src={item.img}
                     alt="Image"
                     className="w-full h-72 rounded"
                   />
                   {/* adding black gradient on top of image*/}
-                  <div className="absolute top-48 left-12 w-4/5 bg-white p-8 text-center border border-black">
+                  <div className="md:absolute md:top-48 md:left-12 w-4/5 bg-white p-8 text-center border border-black">
                     <p className="text-gray-700 my-3 text-center underline">
                       {item.category}
                     </p>
@@ -174,15 +174,15 @@ const App = () => {
           ))}
         </div>
 
-        <div className="flex gap-10">
-          <div className="relative w-full mb-40">
+        <div className="md:flex md:gap-10">
+          <div className="md:relative w-full mb-40">
             <LazyLoadImage
               src={popularImg2}
               alt="Image"
-              className="w-full h-96 rounded"
+              className="w-full md:h-96 rounded"
             />
 
-            <div className="absolute top-72 left-20 w-4/5 bg-white p-8 text-center border border-black">
+            <div className="md:absolute md:top-72 md:left-20 md:w-4/5 bg-white p-8 text-center border border-black">
               <p className="text-gray-700 my-3 text-center underline">SKIN</p>
               <p className="text-4xl mb-5 font-new font-semibold">
                 Vivamus placerat Luctus Neque nec Faucibus Vivamus ultricies
@@ -289,21 +289,25 @@ const App = () => {
       <div className=" mt-[43rem] container mx-auto w-full border-t border-dashed border-black mb-20"></div>
 
       <div className="text-center my-20">
-        <button className="py-3 px-8 bg-black text-white rounded-3xl hover:bg-white hover:text-black hover:scale-125 duration-300 ease-in-out">Load More</button>
+        <button className="py-3 px-8 bg-black text-white rounded-3xl hover:bg-white hover:text-black hover:scale-125 duration-300 ease-in-out">
+          Load More
+        </button>
       </div>
 
       {/* instafeed section */}
       <SectionTitle title="Instagram Feed"></SectionTitle>
       <div className="container mx-auto my-20">
-            <div className="flex justify-evenly">
-              <LazyLoadImage src={instaImg1} className="w-80 h-80"/>
-              <LazyLoadImage src={instaImg2} className="w-80 h-80"/>
-              <LazyLoadImage src={instaImg3} className="w-80 h-80"/>
-              <LazyLoadImage src={instaImg4} className="w-80 h-80"/>
-            </div>
-            <div className="w-full my-16">
-              <button className="w-full py-3 px-8 bg-black text-white rounded-3xl hover:bg-white hover:text-black hover:scale-125 duration-300 ease-in-out">Follow Us</button>
-            </div>
+        <div className="flex justify-evenly">
+          <LazyLoadImage src={instaImg1} className="w-80 h-80" />
+          <LazyLoadImage src={instaImg2} className="w-80 h-80" />
+          <LazyLoadImage src={instaImg3} className="w-80 h-80" />
+          <LazyLoadImage src={instaImg4} className="w-80 h-80" />
+        </div>
+        <div className="w-full my-16">
+          <button className="w-full py-3 px-8 bg-black text-white rounded-3xl hover:bg-white hover:text-black hover:scale-125 duration-300 ease-in-out">
+            Follow Us
+          </button>
+        </div>
       </div>
 
       {/* subscribe section */}
--- src/components/shared/SectionTitle.jsx
+++ src/components/shared/SectionTitle.jsx
@@ -1,9 +1,9 @@
 const SectionTitle = ({title}) => {
   return (
     <div className="my-20 container mx-auto flex justify-center items-center gap-0">
-      <div className="h-1 bg-black w-[40rem] mx-auto my-4"></div>
+      <div className="h-1 bg-black md:w-[40rem] mx-auto my-4"></div>
       <p className="text-3xl ">{title}</p>
-      <div className="h-1 bg-black w-[40rem] mx-auto my-4"></div>
+      <div className="h-1 bg-black md:w-[40rem] mx-auto my-4"></div>
     </div>
   );
 };
